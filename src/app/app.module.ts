70ca6d904ebeaed25246ce2305c998a068cb174d
unknown <Administrator@co-it-maheshg1.srichaitanya.local>
2019-08-14 16:56:20 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,63 @@
-import { BrowserModule } from '@angular/platform-browser';
+
+// @ts-ignore
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+// import Http module
+// @ts-ignore
+import { HttpModule} from '@angular/http';
+
+// import ReactiveFormsModule for reactive form
+// @ts-ignore
+import { ReactiveFormsModule } from '@angular/forms';
+
+// import module for Routing.
+// @ts-ignore
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
-import { HeroFormComponent } from './hero-form/hero-form.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+import { AdminService } from './services/admin.service';
+import { ProfileComponent } from './profile/profile.component';
+
+
+
+// @ts-ignore
+import {BrowserModule} from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroFormComponent
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    ProfileComponent
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path : '',
+        component : HomeComponent
+      },
+      {
+        path : 'login',
+        component : LoginComponent
+      },
+      {
+        path : 'signup',
+        component : SignupComponent
+      },
+      {
+        path : 'profile/:adminId',
+        component : ProfileComponent
+      }
+    ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
